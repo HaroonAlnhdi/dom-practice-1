9bec431da76bdd590ac0d8ad75ded3b1238d68d0
Haroon <you@example.com>
2024-07-03 10:31:04 +0300

    work =)

--- src/script.js
+++ src/script.js
@@ -1,1 +1,47 @@
 // Query Selectors
+
+ const sun = document.querySelectorAll('i ')[0]
+ const moon = document.querySelectorAll('i ')[1]
+ const body = document.querySelector('body');
+ console.log(sun)
+ console.log(moon)
+
+
+
+        const darkMode = () => {
+
+          body.className ="darkMood"
+        }
+        
+
+
+        const lightMode = () => {
+            
+            body.className="body"
+        }
+
+        moon.addEventListener('click' , darkMode);
+        sun.addEventListener('click'  ,lightMode);
+
+
+        //bouns :
+
+
+        // For the Sun
+         sun.addEventListener('mouseenter' , () => {
+            sun.style.borderBottom = '3px solid';
+        
+        
+        } );
+        sun.addEventListener('mouseout' , () => {sun.style.borderBottom = ''} )
+
+
+
+        // For the Moon 
+        moon.addEventListener('mouseenter' , () => {
+            moon.style.borderBottom = '3px solid';
+        
+        
+        } );
+        moon.addEventListener('mouseout' , () => 
+            {moon.style.borderBottom = ''} )
